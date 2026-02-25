9cd480bec9b1408a2cd6ee9a5f3be0703d8432ea
Kennon <kennonssauter@gmail.com>
2026-02-25 14:43:28 -0500

    phrasing progress

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const alpha = "qwertyuiop*asdfghjkl*zxcvbnm";
 const bodytag = document.getElementsByTagName("body")[0];
 const phrases = ["BURGER","CIRCLE","NUGGET","BETTER","FASTER","STRONGER","STARDEW","ISAAC","PUNCHOUT","CALAMITY"]
-const choice =  Math.floor(Math.random() * (9 - 0 + 1)) + 0 
+const choice =  phrases[Math.floor(Math.random() * (9 - 0 + 1)) + 0]
+let deathcounter = 0 
 var counter = 0
 function genKeyboard(){
   for (let i=0; i < 4; i++){
@@ -20,14 +21,14 @@ function genKeyboard(){
     }
     counter++
     if (i == 3) {
-      phrase = phrases[choice]
-      console.log(phrase)
-      length = phrase.length
+      console.log(choice)
+      length = choice.length
       holder = document.getElementsByClassName("in_and_out")[0]
       console.log(length)
       for(let i= 0; i < length; i++) {
         let newletter = document.createElement("div");
         newletter.className = "letter";
+        newletter.id = i
         holder.appendChild(newletter);
         console.log("increased div amount")
       }
@@ -37,6 +38,30 @@ function genKeyboard(){
 }
 function keyPress(id) {
   let output = document.getElementsByTagName("div")[3];
-  output.textContent = output.textContent + id
+  index = []
+  id = id.toUpperCase();
+  length = choice.length
+  if (choice.includes(id)) {
+    console.log("yo")  
+    for(let i= 0; i < length; i++) {
+      if (choice[i] == id) {
+        index.push(i)
+        
+      }
+      console.log(id)
+      console.log(choice[i])
+    }  
+    console.log(counter)
+    console.log(index)
+  }
+  else {
+    id = id.toLowerCase()
+    const key = document.getElementById(id);
+    key.style.backgroundColor = "red";
+    key.style.cursor = "auto";
+    deathcounter +=1
+    guy = document.getElementById(`w${[deathcounter]}`)
+    guy = guy.style.visibility.visible
+  }
 }
 
